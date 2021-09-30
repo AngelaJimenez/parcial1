bdc019240aa9343e288dbd51741d8948959dbc91
AngelaJimenez <al.jimenezf@uniandes.edu.co>
2021-09-29 19:40:23 -0500

    avances que no quiero perder :3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 carrito = [];
-
+confirmation=[];
 const URL =   "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json";
 
 
@@ -25,7 +25,10 @@ function quitarElementos() {
     node.removeChild(node.firstChild);
   }
 }
-
+t((datos) => {
+    comida = datos[0].products;
+    agregar(comida, "Burgers");
+  });
 //Interacción
 let burguers = document.getElementById("burguers");
 burguers.onclick = () => {
@@ -72,8 +75,190 @@ drinks.onclick = () => {
   });
 };
 
+let carritopic = document.getElementById("carritoFoto");
+carritopic.onclick = () => {
+    let divNombre = document.getElementById("titulotipoProducto");
+    let nom = document.createElement("h1");
+    nom.innerText = "Order detail";
+    nom.className = "text-center";
+    divNombre.appendChild(nom);
+    creartablacarrito()    
+};
 
-//PRODUCTOS
+function creartablacarrito(){
+
+    quitarElementos();
+    let totaltotal=0
+    let divtable = document.getElementById("productos");
+
+    let table = document.createElement("table");
+    table.className='table table-striped';
+    table.setAttribute('id', 'tablacarrito')
+    let thead = document.createElement("thead");
+    let tr = document.createElement("tr");
+    let th1 = document.createElement("th");
+    let th2 = document.createElement("th");
+    let th3 = document.createElement("th");
+    let th4 = document.createElement("th");
+    let th5 = document.createElement("th");
+    let th6 = document.createElement("th");
+
+    th1.textContent='Item'
+    th2.textContent='Qty.'
+    th3.textContent='Description'
+    th4.textContent='Unit Price'
+    th5.textContent='Amount'
+    th6.textContent='Modify'
+    
+
+    tr.appendChild(th1)
+    tr.appendChild(th2)
+    tr.appendChild(th3)
+    tr.appendChild(th4)
+    tr.appendChild(th5)
+    tr.appendChild(th6)
+
+    thead.appendChild(tr)
+    table.appendChild(thead);
+
+    divtable.appendChild(table);
+
+
+
+    i=1
+    
+    var setofcarrito = new Set();
+    carrito.forEach(element=> {
+            setofcarrito.add(element)
+    });
+
+    setofcarrito.forEach(food=>{
+        
+        let tr = document.createElement('tr')
+        let td1 = document.createElement('th')
+        let td2 = document.createElement('td')
+        let td3 = document.createElement('td')
+        let td4 = document.createElement('td')
+        let td5 = document.createElement('td')
+        let td6 = document.createElement('td')
+        let divbtn = document.createElement('div')
+        let boton1 = document.createElement("a");
+        divbtn.className="d-grid gap-2"
+        boton1.className = "btn btn-dark";
+        boton1.innerText = "+";
+        let boton2 = document.createElement("a");
+        boton2.className = "btn btn-dark";
+        boton2.innerText = "-";
+        boton1.style="margin:1px"
+
+        divbtn.appendChild(boton1);
+        divbtn.appendChild(boton2);
+        td6.appendChild(divbtn)
+        boton1.onclick = () => {
+            carrito.push(food);
+            confirmation=[];
+            creartablacarrito();
+            getNumeroItems(carrito.length + " Items");
+          };
+        boton2.onclick = () => {
+            carrito.pop(food);
+            confirmation=[];
+            creartablacarrito();
+            getNumeroItems(carrito.length + " Items");
+          };
+        
+        td1.textContent=i
+        i+=1
+        var cantidad=0
+        var total =0
+        carrito.forEach(countable=>{
+            if(countable.name=== food.name)
+            {
+                cantidad+=1
+            }
+        })
+        
+        total=cantidad*food.price
+        td2.textContent=cantidad
+        td3.textContent = food.description
+        td4.textContent = food.price
+        td5.textContent = total
+        totaltotal+=total
+
+        var id = i-1;
+        var quantity = cantidad;
+        var descripcion = food.description;
+        var unitprice = food.price;
+        var obj = {'id': id, 'quantity': quantity,'description':descripcion,'unitprice':unitprice};
+        confirmation.push(obj);
+
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+        tr.appendChild(td3)
+        tr.appendChild(td4)
+        tr.appendChild(td5)
+        tr.appendChild(td6)
+
+        table.appendChild(tr)
+    
+    });
+    let finalconteiner = document.createElement("div");
+
+    finalconteiner.className="container";
+
+    let finalrow = document.createElement("div");
+    finalrow.className="row"
+    let total =  document.createElement("div");
+    total.className="col-9"
+    let rowbotonesfinales=document.createElement("div");
+    rowbotonesfinales.className="col-3"
+
+    let divbotonesfinales=document.createElement("div");
+    
+    let botoncancel = document.createElement("a");
+    botoncancel.className = "btn btn-danger";
+    botoncancel.innerText = "Cancel";
+
+    let botonconfirm = document.createElement("a");
+    botonconfirm.className = "btn btn-success";
+    botonconfirm.innerText = "Confirm Order";
+    
+
+    botoncancel.onclick = () => {
+      openmodal();
+    };
+
+    botonconfirm.onclick = () => {
+      console.log(confirmation)
+      carrito=[];
+      confirmation=[];
+      creartablacarrito();
+      getNumeroItems(carrito.length + " Items");
+
+    };
+
+
+    divbotonesfinales.className="d-grid gap-2"
+    divbotonesfinales.appendChild(botoncancel);
+    divbotonesfinales.appendChild(botonconfirm);
+
+    rowbotonesfinales.appendChild(divbotonesfinales)
+    let p= document.createElement("p");
+    let b= document.createElement("b");
+    b.innerText= "Total: $"+totaltotal
+    p.appendChild(b)
+    total.appendChild(p)
+    finalrow.appendChild(total)
+    finalrow.appendChild(rowbotonesfinales)
+    finalconteiner.appendChild(finalrow)
+    divtable.appendChild(finalconteiner)
+
+}
+
+
+
+
+
 function agregar(comida, titulo) {
     let divProductos = document.getElementById("productos");
     let divNombre = document.getElementById("titulotipoProducto");
@@ -128,9 +313,11 @@ function agregar(comida, titulo) {
           };
     });
 
-    function getNumeroItems(texto)
-    {
-        let item = document.getElementById("numeroProductosCarro");
-        item.innerHTML = texto;
-    }
+  }
+
+  
+  function getNumeroItems(texto)
+  {
+      let item = document.getElementById("numeroProductosCarro");
+      item.innerHTML = texto;
   }
